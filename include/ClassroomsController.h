e8a2c36d5ea848aaf3bc8fdb09e804a34b663f95
lbal-biomat <lbal-biomat>
2022-04-07 22:34:54 -0300

    Add isAvailable function in classroomController

--- include/ClassroomsController.h
+++ include/ClassroomsController.h
@@ -19,7 +19,19 @@ class ClassroomsController {
     CoursesRepo& repoCourses = CoursesRepo::GetInstance();
   public:
     void addClassroom(int num, int capacity);
-    void addClassroomReservation(int numRoom, int codeCourse, int startTime, int endTime, TDate startDate,
+
+    /*Responsibility: returns true if the classroom c such that c.getNumber() == num is available for the given days,
+      date and time, returns false otherwise
+     * Pre: existsClassroom(num) */
+    bool isAvailable(int num, int startTime, int endTime, TDate startDate,
+                     TDate endDate, std::vector<DayOfWeek>& days);
+
+    /* Responsibility: Adds a reservation with the given information.
+     * Pre: existsClassroom(num)
+     * Pre: there is a course c in the system such that c.getCode() == codeCourse
+     * Pre: isAvailable(num, startTime, endTime, startDate, endDate, days)
+     * Throws an invalid_argument exception if the classroom is not available */
+    void addClassroomReservation(int num, int codeCourse, int startTime, int endTime, TDate startDate,
                                  TDate endDate, std::vector<DayOfWeek>& days);
     void printReservations(int numRoom);
     [[nodiscard]] bool existsClassroom(int classNum) const;
