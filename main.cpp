a47e7c1526fc321f051671aefd5cb0afc37251e7
lbal-biomat <lbal-biomat>
2022-04-08 21:48:14 -0300

    Revert "Revert "Refactor main to use Menu class""
    This reverts commit 06892daeefcfa1f47882b176a9b1795e6366e2af.

--- main.cpp
+++ main.cpp
@@ -1,41 +1,26 @@
 
 #include "include/adminInterface.h"
 #include "include/studentInterface.h"
-#include "include/StudentsController.h"
-#include "include/CoursesController.h"
-#include "include/ClassroomsController.h"
+#include "include/Menu.h"
 
+
+void runAsStudent() {
+  int id = studentInterface::studentLogIn();
+  if (id == -1) {
+    return;
+  }
+  studentInterface st(id);
+  studentInterface::callMenu(st.menu, "Log Out");
+}
 
 int main() {
-  ClassroomsController classroomsController;
-  CoursesController coursesController;
-  StudentsController studentsController;
-  adminInterface ad(studentsController, classroomsController, coursesController);
-  studentInterface st(studentsController, coursesController);
-  Interface *inter;
-  bool exit_all = false;
-  while (!exit_all) {
-    int val;
-    std::cout << " \n0-Login as admin, 1-Login as student, 2-Exit\n";
-    std::cin >> val;
-    if (val == 2) {
-      exit_all = true;
-    }
-    else {
-      if (val == 0) {
-        inter = &ad;
-      }
-      else if (val == 1) {
-        bool valid = st.studentLogIn();
-        if (!valid) { continue;} //go back to main menu
-        inter = &st;
-      }
-      else {
-        std::cerr << "Not an option, try again.\n";
-        continue;
-      }
-      inter->callMenu();
-    }
-  }
+  adminInterface ad;
+  Menu menu = {{{0, {"Exit", []{return ;}}},
+                {1, {"Login is admin", [ad]{adminInterface::callMenu(ad.menu, "Log Out");}}},
+                {2, {"Login is student", []{ runAsStudent();}}},
+                }};
+
+  Interface::callMenu(menu, "Exit");
+
   return 0;
 }
