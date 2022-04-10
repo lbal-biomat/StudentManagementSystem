f2d058218a0e313d02279d12c7240a964057d338
lbal-biomat <lbal-biomat>
2022-04-09 22:30:15 -0300

    Add DTStudent class for passing students information

---
+++ src/DTStudent.cpp
@@ -0,0 +1,24 @@
+//
+// Created by lucia on 9/4/22.
+//
+
+#include "../include/DTStudent.h"
+
+#include <utility>
+
+DTStudent::DTStudent(int ID, std::string nom) : id(ID), name(std::move(nom)) {}
+
+int DTStudent::getID() const {
+  return id;
+}
+
+std::string DTStudent::getName() {
+  return name;
+}
+
+
+std::ostream& operator<<(std::ostream& os, DTStudent& e) {
+  os << "Name: " << e.getName() << "\n";
+  os << "ID: " << e.getID() << "\n";
+  return os;
+}
